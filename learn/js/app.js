e40c562b07786db9a294539d8b7828fb954d13de
Bahaa Husari <71553270+bahaahusari@users.noreply.github.com>
2020-12-03 12:23:15 +0200

    Update index.html

--- learn/js/app.js
+++
@@ -1,9 +0,0 @@
-var myApp = angular.module('myApp',[])
-
-myApp.controller('mainController', function ($scope) {
-    $scope.name = 'Mohamad Husari';
-    $scope.getname = function () {
-        return $scope.name;
-    };
-    console.log($scope);
-});
